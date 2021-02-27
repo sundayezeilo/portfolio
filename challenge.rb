bf02cd10fee9572ac3b0a11ac1eb9e3960304264
ezeilo-su <ezeilo.su@gmail.com>
2021-02-27 16:36:59 +0100

    Update skills list

--- challenge.rb
+++ challenge.rb
@@ -1,29 +1,36 @@
 def find_range(num, min, max)
   arr_of_range = []
-  old_not_found = 0
-  new_not_found = 0
-  (min...max).each do |n|
+  prev_not_found = nil
+  next_not_found = nil
+
+  (min..max).each do |n|
     if(num.include?(n))
-      if old_not_found == nil
+      if prev_not_found == nil
         next
-      elsif new_not_found != nil
-        arr_of_range << "#{old_not_found} => #{new_not_found}"
-        old_not_found = nil
-        new_not_found = nil
+      elsif next_not_found != nil
+        arr_of_range << "#{prev_not_found} => #{next_not_found}"
       else
-        arr_of_range << "#{old_not_found}"
-        old_not_found = nil
-        new_not_found = nil
+        arr_of_range << prev_not_found.to_s
       end
+      prev_not_found = nil
+      next_not_found = nil
+    elsif prev_not_found != nil
+      next_not_found = n
     else
-      if old_not_found != nil
-        old_not_found = n
-      else
-        new_not_found = n
-      end
-    end    
+      prev_not_found = n
+    end
   end
+
+  if prev_not_found && next_not_found
+    arr_of_range << "#{prev_not_found} => #{next_not_found}"
+  end
+
   arr_of_range
 end
 
-p find_range([1, 4, 10], 1, 11)
+lower = 2
+upper = 20
+
+p find_range([1, 4, 10, 12, 14, 17], lower, upper)
+
+# ["2 => 3", "5 => 9", "11", "13", "15 => 16", "18 => 20"]
